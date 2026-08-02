60727bdddc1cb20990a8e48e1e9d9032eab1fc5a
poojasri107 <spoojasri2006@gmail.com>
2026-08-02 12:55:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Reverse Words/reverse-words.java
@@ -0,0 +1,20 @@
+class Solution {
+    String reverseWords(String s) {
+
+        String arr[] = s.split("\\.");
+        String ans = "";
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+
+            if (!arr[i].equals("")) {
+
+                if (ans.equals(""))
+                    ans = arr[i];
+                else
+                    ans = ans + "." + arr[i];
+            }
+        }
+
+        return ans;
+    }
+}
